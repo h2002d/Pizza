af5f74ad9f47d2bd39dcf418a791cd71737e25e4
h2002d <davit.harutyunyan1996@gmail.com>
2018-08-12 11:44:43 +0400

    Initializing files

---
+++ LaserArt/Controllers/AdminController.cs
@@ -0,0 +1,54 @@
+﻿using LaserArt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaserArt.Controllers
+{
+    public class AdminController : Controller
+    {
+        public AdminController()
+        {
+            ViewBag.Categories = LaserArt.Models.ParentCategory.GetCategories(null);
+        }
+        // GET: Admin
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Order()
+        {
+            var model=Models.Order.GetOrderById(null);
+            return View(model);
+        }
+
+        
+        [Authorize(Roles = "Administrator")]
+        public ActionResult OrderDetails(int OrderId)
+        {
+            
+            var product = Product.GetProductsByOrderId(OrderId);
+            decimal sum = 0;
+            foreach(var item in product)
+            {
+                sum += item.product.Price * item.ProductQuantity;
+            }
+            var delivery = Models.Order.GetOrderById(OrderId).First().city.Money;
+            sum += delivery;
+            ViewBag.Amount = sum;
+            return View(product);
+        }
+        [Authorize(Roles = "Administrator")]
+        public ActionResult ChangeOrderStatus(int orderId,int status)
+        {
+            Models.Order.ChangeOrderStatus(orderId, status);
+           return RedirectToAction("Order");
+        }
+
+      
+    }
+}
---
+++ LaserArt/Models/JsonObjects.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LaserArt.Models
+{
+    public class JsonObjects
+    {
+    }
+    public class RegisterResponse
+    {
+        public int errorCode { get; set; }
+        public string errorMessage { get; set; }
+        public string orderId { get; set; }
+        public string formUrl { get; set; }
+    }
+    public class ApproveResponse
+    {
+        public int errorCode { get; set; }
+        public string OrderNumber { get; set; }
+        public string OrderStatus { get; set; }
+    }
+}
